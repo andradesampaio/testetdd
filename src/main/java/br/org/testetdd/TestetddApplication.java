6af0bb241dba9494735d7e24bdfa900159c7d373
Andrade Sampaio <andrade.sampaio@outlook.com>
2019-07-16 20:10:23 -0300

    Preparando Ambiente

---
+++ src/main/java/br/org/testetdd/TestetddApplication.java
@@ -0,0 +1,13 @@
+package br.org.testetdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestetddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestetddApplication.class, args);
+	}
+
+}
