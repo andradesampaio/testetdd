9eaf85adac8a2a7add142d02fc1dd6bb4544f0c6
Andrade Sampaio <andrade.sampaio@outlook.com>
2019-07-23 11:15:07 -0300

    Implementando metodos novos

--- src/main/java/br/org/testetdd/leilao/Avaliador.java
+++ src/main/java/br/org/testetdd/leilao/Avaliador.java
@@ -1,18 +1,48 @@
 package br.org.testetdd.leilao;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class Avaliador {
 
     private double maiorLance = Double.NEGATIVE_INFINITY;
+    private double menorLance = Double.POSITIVE_INFINITY;
+    private List<Lance> maioresLances;
+    private List<Lance> listMaioresLances;
 
-    public void avalia(Leilao leilao){
-        for (Lance lance: leilao.getLances()){
-            if(lance.getValor() > maiorLance) maiorLance = lance.getValor();
-
+    public void avalia(Leilao leilao) {
+        if(leilao.getLances().size() == 0){
+            throw new RuntimeException("Nao e possivel avaliar sem lances");
         }
+
+        for (Lance lance : leilao.getLances()) {
+            if (lance.getValor() > maiorLance) maiorLance = lance.getValor();
+            if (lance.getValor() < menorLance) menorLance = lance.getValor();
+        }
+
+
+      maioresLances = new ArrayList<Lance>(leilao.getLances());
+
+        listMaioresLances = maioresLances.stream()
+                .sorted(Comparator.comparingDouble(Lance::getValor)
+                .reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+
     }
 
-    public double getMaiorLance(){
+    public List<Lance> getListMaiores(){
+        return listMaioresLances;
+    }
+        public double getMaiorLance() {
         return maiorLance;
+    }
+
+    public double getMenorLance() {
+        return menorLance;
     }
 
 }
